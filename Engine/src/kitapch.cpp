bafc37a4230835c1c95cfb65540f5c8ec7d3ad39
Vojtěch Šokala <vojtasokala@gmail.com>
2025-02-16 10:31:43 +0100

    Add precompiled header

---
+++ Engine/src/kitapch.cpp
@@ -0,0 +1,1 @@
+#include "kitapch.h"
---
+++ Engine/src/kitapch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef NOMINMAX
+    //github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
+    #define NOMINMAX
+#endif
+
+#include <Windows.h>
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "Logger.h"
