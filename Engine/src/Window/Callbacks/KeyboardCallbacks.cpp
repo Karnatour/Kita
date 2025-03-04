7754a713f2e2fb88ca255536e468ee8ec53c6958
Vojta <vojtasokala@gmail.com>
2025-03-04 14:13:55 +0100

    forgot to commit KeyboardCallbacks

---
+++ Engine/src/Window/Callbacks/KeyboardCallbacks.cpp
@@ -0,0 +1,27 @@
+#include "../../kitapch.h"
+#include "KeyboardCallbacks.h"
+#include "../../Events/EventManager.h"
+#include "../../Events/KeyboardEvents.h"
+#include "../../Input/InputKeys.h"
+
+namespace Kita {
+    void keyboardKeyCallbackFun(GLFWwindow* window, int key, int scancode, int action, int mods) {
+        KeyPressed pressedEvent(convertGLFWToKitaKey(key), mods);
+        KeyReleased releasedEvent(convertGLFWToKitaKey(key));
+        switch (action) {
+        case GLFW_PRESS:
+            EventManager::triggerEvent(pressedEvent);
+            break;
+        case GLFW_RELEASE:
+            EventManager::triggerEvent(releasedEvent);
+            break;
+        case GLFW_REPEAT:
+            //Emulate REPEAT as keyPreesed event
+            EventManager::triggerEvent(pressedEvent);
+            break;
+        default:
+            KITA_ENGINE_WARN("keyboardKeyCallbackFun recived unknown action");
+            break;
+        }
+    }
+}
---
+++ Engine/src/Window/Callbacks/KeyboardCallbacks.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+
+namespace Kita {
+    void keyboardKeyCallbackFun(GLFWwindow* window, int key, int scancode, int action, int mods);
+}
